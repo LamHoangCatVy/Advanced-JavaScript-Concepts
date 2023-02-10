382e9fc54a13d3363196dcd04f769abe1429f937
LamHoangCatVy <catvyisstudying@gmail.com>
2023-02-11 01:50:37 +0700

    a little bit clear with bind()

---
+++ 15-Bind - start/README.md
@@ -0,0 +1,5 @@
+Using bind...
+-- var func = function.bind(this, arg1, arg2);
+-- bind returns a new function
+-- The first argument is an object that will become the value of this for that new function
+-- One or more arguments can be included that will be bound to the new function, meaning you will not need to pass in those arguments.
--- 15-Bind - start/app.js
+++ 15-Bind - start/app.js
@@ -1,20 +1,31 @@
 var user1 = {
-    firstName: "John",
-    lastName: "Anderson",
-    fullName: function() {
-        return this.firstName + " " + this.lastName;
-    }
+  firstName: "John",
+  lastName: "Anderson",
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  },
 };
 
 var user2 = {
-    firstName: "Sarah",
-    lastName: "West",
-    fullName: function() {
-        return this.firstName + " " + this.lastName;
-    }
+  firstName: "Sarah",
+  lastName: "West",
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  },
 };
 
-var greeting = function(term, punct) {
-    console.log(term + " " + this.firstName + punct);
+var greeting = function (term, punct) {
+  console.log(term + " " + this.firstName + punct);
 };
 
+var morningGreet = greeting.bind(user1, "Good Morning");
+var afternoonGreet = greeting.bind(user1, "Good Afternoon");
+
+morningGreet("!");
+//Good Morning John!
+afternoonGreet(".");
+//Good Afternoon John!
+
+morningGreet.call(user2, "!")
+//cannot change this because bind() take precedence
+//Result will still be "Good Morning John!"
