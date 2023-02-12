6ce0d8f92d626156ac8be77fa614e4f741fa1c6b
Lam Hoang Cat Vy <catvyisstudying@gmail.com>
2023-02-13 05:24:31 +0700

    finished exercise 2 with 3 ways to know what are the types of values' properties

--- Exercise2 - start/app.js
+++ Exercise2 - start/app.js
@@ -1,5 +1,26 @@
+var content = document.querySelector("#content");
 
-var content = document.querySelector('#content');
+content.innerHTML =
+  "President " +
+  pres.fullName() +
+  " was president number " +
+  pres.order +
+  " of the United States of America.";
 
-content.innerHTML = "President " + pres.fullName() + " was president number " + pres.order + " of the United States of America.";
+for (var prop in pres) {
+  console.log(prop + " is " + typeof pres[prop]);
+}
 
+console.log("_____Another way to know if object have OWNED Property_____");
+for (var prop in pres) {
+  if (pres.hasOwnProperty(prop)) {
+    console.log(prop + "is " + typeof pres[prop]);
+  }
+}
+
+console.log("_____One more to know if object have OWNED Property_____");
+var arr = Object.keys(pres);
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i] + " is " + typeof pres[arr[i]]);
+}
--- Exercise2 - start/js/main.js
+++ Exercise2 - start/js/main.js
@@ -1,35 +1,48 @@
-
 var pres = {
-    firstName: "James",
-    midName: "Abram",
-    lastName: "Garfield",
-    assisination: true,
-    born: "November 19, 1831",
-    died: "September 19, 1881",
-    party: "Republican",
-    termStart: "March 4, 1881",
-    termEnd: "September 19, 1881",
-    vp: "Chester Arthur",
-    quotes: ["If wrinkles must be written upon our brows, let them not be written upon the heart. The spirit should not grow old.", "The truth will set you free, but first it will make you miserable.", "A pound of pluck is worth a ton of luck."],
-    order: 20
+  firstName: "James",
+  midName: "Abram",
+  lastName: "Garfield",
+  assisination: true,
+  born: "November 19, 1831",
+  died: "September 19, 1881",
+  party: "Republican",
+  termStart: "March 4, 1881",
+  termEnd: "September 19, 1881",
+  vp: "Chester Arthur",
+  quotes: [
+    "If wrinkles must be written upon our brows, let them not be written upon the heart. The spirit should not grow old.",
+    "The truth will set you free, but first it will make you miserable.",
+    "A pound of pluck is worth a ton of luck.",
+  ],
+  order: 20,
 };
 
 var presProto = {
-    fullName() {
-        return this.firstName + " " + this.midName + " " + this.lastName;
-    },
-    inagurationAge() {
-        var born = new Date(this.born);
-        var inaguration = new Date(this.termStart);
-        return Math.floor((inaguration - born) / (1000 * 60 * 60 * 24 * 365));
-    },
-    daysServed() {
-        var end = new Date(this.termEnd),
-            start = new Date(this.termStart);
+  fullName() {
+    return this.firstName + " " + this.midName + " " + this.lastName;
+  },
+  inagurationAge() {
+    var born = new Date(this.born);
+    var inaguration = new Date(this.termStart);
+    return Math.floor((inaguration - born) / (1000 * 60 * 60 * 24 * 365));
+  },
+  daysServed() {
+    var end = new Date(this.termEnd),
+      start = new Date(this.termStart);
 
-        return Math.floor((end - start) / (1000 * 60 * 60 *24));
-    }
+    return Math.floor((end - start) / (1000 * 60 * 60 * 24));
+  },
 };
 
 Object.setPrototypeOf(pres, presProto);
 
+// //display each properties' name and the properties' values
+// for (var prop in pres) {
+//   console.log("Name: " + prop);
+//   console.log("Value: " + pres[prop]);
+// }
+
+
+// //Display arrays of pres's properties
+// var properties = Object.keys(pres);
+// console.log(properties);
