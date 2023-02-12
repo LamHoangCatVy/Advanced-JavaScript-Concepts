0ba8d49e4336b9be7a735873d5af835d995bc6a9
Lam Hoang Cat Vy <catvyisstudying@gmail.com>
2023-02-13 04:05:30 +0700

    Update app.js

--- 21-Detect Properties - start/app.js
+++ 21-Detect Properties - start/app.js
@@ -1,11 +1,30 @@
-var  person1 = {
-    firstName: "Steven",
-    lastName: "Hancock",
-    email: "shancock@allthingsjavascript.com",
-    type: "admin",
-    active: true,
-    address: {
-        street: "100 N. Main",
-        zip: 10001
-    }
+var person1 = {
+  firstName: "Steven",
+  lastName: "Hancock",
+  email: "shancock@allthingsjavascript.com",
+  type: "admin",
+  active: true,
+  address: {
+    street: "100 N. Main",
+    zip: 10001,
+  },
 };
+
+
+for (var prop in person1) {
+  console.log("Name: " + prop);
+  console.log("Value: " + person1[prop]);
+}
+//Name: firstName, lastName,...
+//Value: Steven, Hancock,...
+
+
+person1.propertyIsEnumerable("firstname");
+//true
+person1.propertyIsEnumerable("toString");
+//false
+
+
+var properties = Object.keys(person1);
+console.log(properties)
+//["firstName", "lastName",.....]
